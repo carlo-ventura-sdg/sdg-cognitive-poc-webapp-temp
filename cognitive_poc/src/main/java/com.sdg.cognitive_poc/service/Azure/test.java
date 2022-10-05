438662e8236e36f296af1e52f4528704799f095a
Carlo Ventura <venturac@sdg.local>
2022-10-05 10:52:38 +0200

    rollback

--- cognitive_poc/src/main/java/com.sdg.cognitive_poc/service/Azure/test.java
+++
@@ -1,4 +0,0 @@
-package com.sdg.cognitive_poc.service.Azure;
-
-public class test {
-}
--- cognitive_poc/src/main/java/com.sdg.cognitive_poc/service/AzureFeign/Cognitive.java
+++
@@ -1,9 +0,0 @@
-package com.sdg.cognitive_poc.service.AzureFeign;
-
-import com.sdg.cognitive_poc.service.AzureFeign.FeignClient.response.Search.SearchResponse;
-
-public interface Cognitive {
-
-    SearchResponse Search(String search, String apiVersion);
-
-}
